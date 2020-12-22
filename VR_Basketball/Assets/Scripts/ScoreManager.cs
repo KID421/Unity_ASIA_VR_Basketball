c703c686745ea43a282fd8b0861b2f4aa79b1e92
KID421 <50666207+KID421@users.noreply.github.com>
2020-12-22 11:57:47 +0800

    添加程式判定

---
+++ VR_Basketball/Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;   // 引用 介面 API
+
+public class ScoreManager : MonoBehaviour
+{
+    [Header("分數介面")]
+    public Text textScore;
+    [Header("分數")]
+    public int score;
+
+    // OTE 條件：
+    // 兩個碰撞物件 其中一個 必須勾選 IsTrigger
+    // 要偵測此腳本子物件是否產生碰撞
+    // 必須添加剛體 Rigidbody
+    private void OnTriggerEnter(Collider other)
+    {
+        print(other.name);
+    }
+}
